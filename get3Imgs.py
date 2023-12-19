a6a60079974ad0e63745cf69e661cfcc05697d17
Joona Helenius <joalhel@utu.fi>
2023-12-19 15:00:59 +0200

    Add curve and point work in mainEditCurves. Other modes don't work yet

--- get3Imgs.py
+++ get3Imgs.py
@@ -24,7 +24,7 @@ def get_3_imgs(m):
         if i == 0 or i == m - 1 or i == total_frames - 1:
             img0 = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             img1 = ~cv2.Canny(img0, 3, 1.8)
-            imgs.append({'img0': img0.tolist(), 'img1': img0.tolist(), 'pss': [{'ps':[], 'ori': 0}], 'corners': []})
+            imgs.append({'img0': img0.tolist(), 'img1': img0.tolist(), 'pss': [], 'corners': []})
 
     cap.release()
     return imgs
