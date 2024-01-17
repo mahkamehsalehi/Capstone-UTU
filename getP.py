1ec21dbf068f0c3d40fe8c6ab4da4366f16bd3da
Ottispottis <osheld@utu.fi>
2024-01-17 15:27:41 +0200

    debugging get points and mainEditCurves

--- getP.py
+++ getP.py
@@ -5,11 +5,14 @@ def get_p(pss, p, d_max):
     i = 0
     j = 0
     d = np.inf
-
+    print(pss)
     for i0, ps in enumerate(pss):
-        n = len(ps)
+        n = len(ps['ps'])
+        print('i0: ',i0)
+        print('N: ',n)
         if n > 0:
-            tree = KDTree(ps)
+            print('ps: ',ps['ps'])
+            tree = KDTree(ps['ps'])
             ji, di = tree.query([p], k=1)
 
             if di < d:
