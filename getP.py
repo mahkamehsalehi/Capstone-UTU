87919866241f149fc5d43dca6dc12e886e3f03e8
Joona Helenius <joalhel@utu.fi>
2024-02-06 16:34:46 +0200

    Changed getP to use KNN and not KDTree

--- getP.py
+++ getP.py
@@ -1,20 +1,20 @@
 import numpy as np
-from scipy.spatial import KDTree
+from sklearn.neighbors import NearestNeighbors
 
 def get_p(pss, p, d_max):
     i = 0
     j = 0
     d = np.inf
-    print(pss)
     for i0, ps in enumerate(pss):
         n = len(ps['ps'])
         print('i0: ',i0)
         print('N: ',n)
         if n > 0 and p.size>0:
             print('ps: ',ps['ps'])
-            tree = KDTree(ps['ps'])
-            ji, di = tree.query([p], k=1)
-
+            neigh = NearestNeighbors(n_neighbors = 1, metric='euclidean')
+            neigh.fit(ps['ps'])
+            di, ji = neigh.kneighbors(p)
+            print(di, ji)
             if di < d:
                 d = di[0]
                 i = i0
