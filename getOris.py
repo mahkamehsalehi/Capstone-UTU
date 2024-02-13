a6b72386fef53c298ac1bde45b52f89653876049
Ottispottis <osheld@utu.fi>
2024-02-13 16:31:58 +0200

    get oris fixes

--- getOris.py
+++ getOris.py
@@ -3,11 +3,10 @@ def getNewOris(pss):
     Extracts 'ori' values from a list of objects 'pss'.
     Returns two lists: all 'ori' values and unique 'ori' values.
     """
-
-    # Extract 'ori' values from each object in the list.
-    allOris = [obj.ori for obj in pss]
-
+    # Extract 'ori' values from each object in the list, if it is a dictionary with 'ori' key.
+    allOris = [obj['ori'] for obj in pss if isinstance(obj, dict) and 'ori' in obj]
+    
     # Find unique 'ori' values.
     uniqueOris = list(set(allOris))
-
     return allOris, uniqueOris
+
